afe2393a6aba285b372457a1c32622e0f31a4932
aleksandrtikhonov <aleksandrtikhonov91@gmail.com>
2022-03-24 01:06:49 +0300

    rerun git_actions

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 ![yamdb_workflow](https://github.com/aleksandrtikhonov/yamdb_final/workflows/yamdb_workflow/badge.svg)
 test
+test
--- api_yamdb/migrations_and_static.sh
+++ api_yamdb/migrations_and_static.sh
@@ -1,3 +1,3 @@
-docker-compose exec web python manage.py makemigrations reviews
-docker-compose exec web python manage.py migrate
-docker-compose exec web python manage.py collectstatic --no-input
+python manage.py makemigrations reviews
+python manage.py migrate
+python manage.py collectstatic --no-input
